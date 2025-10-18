6cd0b68396f5f1ba71b0e29ece0d203fea814d4a
Pablo Sosa <sosadavid4280@gmail.com>
2025-10-18 13:45:12 -0600

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import express from "express";
+import { database } from "./config/database.js";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+try {
+  await database();
+
+  app.listen(port, () => {
+    console.log(`El servidor se está ejecutando en: http://localhost:${port}`);
+  });
+} catch (error) {
+  console.error("Error al ejecutar el servidor:", error);
+}
