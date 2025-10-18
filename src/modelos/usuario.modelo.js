986217c53c0016e59301e63557019c51aab4ffe5
Pablo Sosa <sosadavid4280@gmail.com>
2025-10-18 15:43:27 -0600

    Add User model

---
+++ src/modelos/usuario.modelo.js
@@ -0,0 +1,23 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/database.js";
+
+export const Usuario = sequelize.define(
+  "Usuario",
+  {
+    nombre: { type: DataTypes.STRING, allowNull: false },
+    apellido: { type: DataTypes.STRING, allowNull: false },
+    correo: { type: DataTypes.STRING, allowNull: false, unique: true },
+    contrasena: { type: DataTypes.STRING, allowNull: false },
+    permiso: {
+      type: DataTypes.ENUM("admin", "empleado", "cliente"),
+      defaultValue: "cliente",
+    },
+  },
+  {
+    tableName: "usuarios",
+    timestamps: true,
+    createdAt: "creado",
+    updatedAt: "actualizado",
+    deletedAt: false,
+  },
+);
