b3f41c4018de770ff6c48b0c7bf5d2ae5b9f0d0e
Pablo Sosa <sosadavid4280@gmail.com>
2025-10-25 03:17:34 -0600

    feat: agregar github actions

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-import "dotenv/config";
-import express from "express";
-import { database } from "./config/database.js";
+import 'dotenv/config';
+import express from 'express';
+import { database } from './config/database.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,11 +9,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 try {
-  await database();
+    await database();
 
-  app.listen(port, () => {
-    console.log(`El servidor se está ejecutando en: http://localhost:${port}`);
-  });
+    app.listen(port, () => {
+        console.log(`El servidor se está ejecutando en: http://localhost:${port}`);
+    });
 } catch (error) {
-  console.error("Error al ejecutar el servidor:", error);
+    console.error('Error al ejecutar el servidor:', error);
 }
--- src/config/database.js
+++ src/config/database.js
@@ -1,33 +1,28 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from 'sequelize';
 
-if (
-  !process.env.DB_NAME ||
-  !process.env.DB_USER ||
-  !process.env.DB_PASS ||
-  !process.env.DB_HOST
-) {
-  console.error("Falta una o más variables de entorno de la base de datos");
-  process.exit(1);
+if (!process.env.DB_NAME || !process.env.DB_USER || !process.env.DB_PASS || !process.env.DB_HOST) {
+    console.error('Falta una o más variables de entorno de la base de datos');
+    process.exit(1);
 }
 
 const { DB_NAME, DB_USER, DB_PASS, DB_HOST } = process.env;
 
 export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
-  dialect: "mysql",
-  host: DB_HOST,
-  logging: false,
-  pool: { max: 5, min: 0, acquire: 30000, idle: 10000 },
+    dialect: 'mysql',
+    host: DB_HOST,
+    logging: false,
+    pool: { max: 5, min: 0, acquire: 30000, idle: 10000 },
 });
 
 export async function database() {
-  try {
-    await sequelize.authenticate();
-    console.log("Conexión a la base de datos establecida con éxito");
+    try {
+        await sequelize.authenticate();
+        console.log('Conexión a la base de datos establecida con éxito');
 
-    await sequelize.sync({ force: true });
-    console.log("Modelos de base de datos sincronizados correctamente");
-  } catch (error) {
-    console.error("Error al conectar la base de datos:", error);
-    process.exit(1);
-  }
+        await sequelize.sync({ force: true });
+        console.log('Modelos de base de datos sincronizados correctamente');
+    } catch (error) {
+        console.error('Error al conectar la base de datos:', error);
+        process.exit(1);
+    }
 }
